1f7ca8b01f59c1b1af0ebf32c6484bd51cb80072
Nate Clark <natec425@gmail.com>
2019-01-17 14:13:37 -0600

    Add test for return_value method

--- tests/test_func_form.py
+++ tests/test_func_form.py
@@ -2,6 +2,10 @@ from dj_func_forms import FuncForm
 from django.forms import CharField, IntegerField, BooleanField
 from django.conf import settings
 
+# This is needed to allow Form instances to be created.
+# I was getting exceptions for django not being configured.
+settings.configure(USE_I18N=False)
+
 
 def test_FuncForm_generates_CharField_for_params_without_annotations():
     def add(x, y):
@@ -34,3 +38,15 @@ def test_FuncForm_infers_field_for_python_builtin_types():
 
     assert isinstance(AddForm.x, IntegerField)
     assert isinstance(AddForm.y, BooleanField)
+
+def test_a_valid_FuncForm_exposes_a_return_value_method():
+    def add(x: int, y: int):
+        return x + y
+
+    class AddForm(FuncForm):
+        function = add
+
+    form = AddForm({'x': '3', 'y': '12'})
+    form.is_valid()
+    assert form.return_value() == 15
+
