f497901356c1babb7a2674402fe90474b733590f
Nate Clark <natec425@gmail.com>
2019-01-17 13:49:21 -0600

    Add test for using Field param annotations

--- tests/test_func_form.py
+++ tests/test_func_form.py
@@ -1,5 +1,5 @@
 from dj_func_forms import FuncForm
-from django.forms import CharField
+from django.forms import CharField, IntegerField
 from django.conf import settings
 
 
@@ -12,3 +12,14 @@ def test_FuncForm_generates_CharField_for_params_without_annotations():
 
     assert isinstance(AddForm.x, CharField)
     assert isinstance(AddForm.y, CharField)
+
+
+def test_FuncForm_will_use_Field_parameter_annotations():
+    def add(x: IntegerField(), y: IntegerField()):
+        return x + y
+
+    class AddForm(FuncForm):
+        function = add
+
+    assert isinstance(AddForm.x, IntegerField)
+    assert isinstance(AddForm.y, IntegerField)
